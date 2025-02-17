2062033b40e4c4838b90d1ac60d8a9bc0bb18565
Maksym Chornomaz <chornomaz.maksym@gmail.com>
2025-02-17 18:04:45 +0200

    add initial structure and setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {CoursesComponent} from './pages/courses/courses.component';
+import {UsersComponent} from './pages/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'courses/breadcrumbs/courses',
+    component: CoursesComponent
+  },
+
+  {
+    path: 'courses/breadcrumbs/users',
+    component: UsersComponent
+  },
+
+  {
+    path: '',
+    redirectTo: '/courses/breadcrumbs/courses',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  imports: [
+    RouterLink
+  ],
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
